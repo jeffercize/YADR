ab44246df5a1c3b79141d6e9ab8ac24d12c7166c
Stephen Banghart <stephen.banghart@outlook.com>
2024-05-02 14:45:00 -0400

    stash

--- scripts/player/Equipment.cs
+++ scripts/player/Equipment.cs
@@ -5,22 +5,53 @@ using System.Collections.Generic;
 [GlobalClass]
 public partial class Equipment : Node
 {
-    public Dictionary<String, EquipSlot> equipSlots = new();
-    private string[] slotStrings = { "helmet", "bodyArmor", "weapon1", "weapon2", "face", "boot", "helmetAttachment", "glove", "backpack", "quick1", "quick2", "quick3" };
-
+    public List<String> defaultSlotNames = new List<string> { "head", "chest", "back", "leftHand", "rightHand" };
+    public Dictionary<String, Equipable> slots = new();
+    public Dictionary<String, List<Equipable.EquipType>> slotTypes = new();
     public Character connectedCharacter;
+
+    public delegate void equipMessageHandler(EquipSlot slot, Item item);
+    public static event equipMessageHandler equipMessage = delegate { };
+
+    public delegate void unequipMessageHandler(EquipSlot slot, Item item);
+    public static event unequipMessageHandler unequipMessage = delegate { };
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        foreach (string s in slotStrings)
-        {
+        initDefaultSlots();
+    }
 
+    private void initDefaultSlots()
+    {
+        foreach (String s in defaultSlotNames)
+        {
+            slots.Add(s, Equipable.NONE);
         }
+
+        slotTypes.Add("head", new List<Equipable.EquipType>{ Equipable.EquipType.HELMET });
+
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
 	{
 	}
+
+    internal void equip(string slotName, Equipable item)
+    {
+        if (slots.TryGetValue("slotName", out Equipable currentEquip))
+        {
+            if (currentEquip == Equipable.NONE)
+            {
+                slots.Remove(slotName);
+                slots.TryAdd(slotName, item);
+            }
+        }
+    }
+
+    internal void unequip(StringName name, Item dragItem)
+    {
+        throw new NotImplementedException();
+    }
 }
