86a063282f0c2d38c150d153dbce071fdaf03dad
Stephen Banghart <stephen.banghart@outlook.com>
2024-05-17 16:39:39 -0400

    git track fix

---
+++ scripts/networking/ClientPlayerStateHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkMessages;
+using static NetworkManager;
+using Google.Protobuf;
+
+public class ClientPlayerStateHandler
+{
+    public delegate void NetworkPlayerStatePositionEventHandler(PlayerStatePositionMessage message);
+    public static event NetworkPlayerStatePositionEventHandler NetworkPlayerStatePositionEvent = delegate { };
+
+    public static void CreateAndSendPlayerPositionMessage(Player player)
+    {
+        PlayerStatePositionMessage message = new PlayerStatePositionMessage();
+        
+        Identity identity = new Identity();
+        identity.SteamID = (long)player.clientID;
+        identity.Name = "";
+        message.PlayerIdentity = identity;
+
+        Position position = new Position();
+        position.X = player.GlobalPosition.X;
+        position.Y = player.GlobalPosition.Y;
+        position.Z = player.GlobalPosition.Z;
+        message.Position = position;
+
+        //NetworkManager.SendSteamMessage(Global.NetworkManager.client.connectionToServer, MessageType.PlayerStatePosition, message);
+        Global.NetworkManager.server.BroadcastMessageWithExclusion((long)Global.instance.clientID,WrapMessage(MessageType.PlayerStatePosition,message));
+
+    }
+
+    public void handlePlayerStatePositionMessage(PlayerStatePositionMessage message)
+    {
+        NetworkPlayerStatePositionEvent.Invoke(message);
+    }
+}
+
